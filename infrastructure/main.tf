08f47d049f14a18a60709ebf6b8e4fdc8b02bc5f
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 18:53:04 -0400

    typo in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,7 @@ terraform {
 resource "null_resource" "deploy_compose" {
 	provisioner "local-exec" {
 		command = <<EOT
-ssh ${var.ssh_user}@{var.ssh_host} <<'REMOTE'
+ssh ${var.ssh_user}@${var.ssh_host} <<'REMOTE'
 	cd ${var.app_dir} &&
 	git pull &&
 	docker-compose -f infrastructure/docker-compose.yaml up -d --build
