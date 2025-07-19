785cecce5081fbd6cdb32217a1fe34b8c9dfac62
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 19:20:43 -0400

    verbose ssh command in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,14 @@
 terraform {
-	backend "local" {}
+  backend "local" {}
 }
 resource "null_resource" "deploy_compose" {
-	provisioner "local-exec" {
-		command = <<EOT
-ssh ${var.ssh_user}@${var.ssh_host} <<'REMOTE'
-	cd ${var.app_dir} &&
-	git pull &&
-	docker-compose -f infrastructure/docker-compose.yaml up -d --build
+  provisioner "local-exec" {
+    command = <<EOT
+ssh -vvv ${var.ssh_user}@${var.ssh_host} <<'REMOTE'
+  cd ${var.app_dir} &&
+  git pull &&
+  docker-compose -f infrastructure/docker-compose.yaml up -d --build
 REMOTE
 EOT
-	}
+  }
 }
