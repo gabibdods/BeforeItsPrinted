e9705653163521cd74dd1a5c9853faacd5e12c0d
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 20:39:26 -0400

    fixed main.tf deployment command

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 resource "null_resource" "deploy_compose" {
   provisioner "local-exec" {
-    command = "ssh -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} \"cd ${var.app_dir} && git pull && docker-compose -f infrastructure/docker-compose.yaml up -d --build\""
+    command = "ssh ${var.ssh_user}@${var.ssh_host} \"cd ${var.app_dir} && git pull && docker compose up -d --build\""
   }
   triggers = {
     always_run = timestamp()
