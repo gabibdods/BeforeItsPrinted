cfa48744dd67081cf3aed892e208039b8d1d428a
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 21:41:00 -0400

    fixed secrets, Dockerfile and more verbose main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "deploy_compose" {
   provisioner "local-exec" {
     command = <<EOT
 echo "Starting remote SSH..."
-ssh -vvv -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} "cd ${var.app_dir} && echo 'In directory' && git pull && echo 'Git pulled' && docker compose up -d --build && echo 'Docker started'"
+ssh -vvv ${var.ssh_user}@${var.ssh_host} "set -e;cd ${var.app_dir} && echo 'In directory' && git pull && echo 'Git pulled' && docker compose up -d --build && echo 'Docker started'"
 echo "SSH command completed"
 EOT
     interpreter = ["PowerShell", "-Command"]
