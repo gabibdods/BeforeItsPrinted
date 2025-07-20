0f18478b7372c8cb2153150e4967c36446a7c762
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 20:47:03 -0400

    added more debugging commands

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,12 @@ terraform {
 }
 resource "null_resource" "deploy_compose" {
   provisioner "local-exec" {
-    command = "ssh -vvv ${var.ssh_user}@${var.ssh_host} \"cd ${var.app_dir} && git pull && docker compose up -d --build\""
+    command = <<EOT
+echo "Starting remote SSH..."
+ssh -vvv -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} "cd ${var.app_dir} && echo 'In directory' && git pull && echo 'Git pulled' && docker compose up -d --build && echo 'Docker started'"
+echo "SSH command completed"
+EOT
+    interpreter = ["PowerShell", "-Command"]
   }
   triggers = {
     always_run = timestamp()
