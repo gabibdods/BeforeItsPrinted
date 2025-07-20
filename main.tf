4de89512bb2ba5d18c8dc9ef11023b7f0c5b3d94
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-20 00:00:37 -0400

    docker compose down before docker compose up -d --build

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
-        command = "ssh ${var.ssh_user}@${var.ssh_host} 'set -e; export PORT=${var.port}; export TROP=${var.trop}; cd ${var.app_dir} && git pull && docker compose up -d --build'"
+        command = "ssh ${var.ssh_user}@${var.ssh_host} 'set -e; export PORT=${var.port}; export TROP=${var.trop}; cd ${var.app_dir} && git pull && docker compose down && docker compose up -d --build'"
         interpreter = ["PowerShell", "-Command"]
     }
     triggers = {
