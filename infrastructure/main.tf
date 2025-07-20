9f453bbe615bda93f6689fc96c66386913fe4177
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 20:16:51 -0400

    tf did not execute main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,8 +3,9 @@ terraform {
 }
 resource "null_resource" "deploy_compose" {
   provisioner "local-exec" {
-    command = <<EOT
-ssh -vvv ${var.ssh_user}@${var.ssh_host} "cd ${var.app_dir} && git pull && docker-compose -f infrastructure\\docker-compose.yaml up -d --build"
-EOT
+    command = "ssh -o StrictHostKeyChecking=no ${var.ssh_user}@${var.ssh_host} \"cd ${var.app_dir} && git pull && docker-compose -f infrastructure/docker-compose.yaml up -d --build\""
+  }
+  triggers = {
+    always_run = timestamp()
   }
 }
