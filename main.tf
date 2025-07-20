7b4f1ea3345999b910e7f78aa8bfb9014f1702d4
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-20 00:31:14 -0400

    debugging

--- main.tf
+++ main.tf
@@ -4,15 +4,21 @@ terraform {
 resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
         command = <<EOT
-        ssh ${var.ssh_user}@${var.ssh_host} 'set -e;
+            echo "Starting SSH command...";
+            ssh -vvv ${var.ssh_user}@${var.ssh_host} 'set -e;
             export POSTGRES_USER="${var.postgres_user}";
             export POSTGRES_PASSWORD="${var.postgres_pass}";
             export PORT="${var.port}";
             export TROP="${var.trop}";
             cd "${var.app_dir}" &&
+            pwd &&
             git pull &&
+            git status &&
             docker compose down &&
-            docker compose up -d --build'
+            docker ps &&
+            docker compose up -d --build' &&
+            docker ps -a;
+            echo "SSH command completed"
         EOT
         interpreter = ["PowerShell", "-Command"]
     }
