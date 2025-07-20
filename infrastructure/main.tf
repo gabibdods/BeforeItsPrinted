9ad2f080455ce21e0a8df48714156aea3af6e3ef
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-19 20:42:25 -0400

    added flags for verbose output

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 resource "null_resource" "deploy_compose" {
   provisioner "local-exec" {
-    command = "ssh ${var.ssh_user}@${var.ssh_host} \"cd ${var.app_dir} && git pull && docker compose up -d --build\""
+    command = "ssh -vvv ${var.ssh_user}@${var.ssh_host} \"cd ${var.app_dir} && git pull && docker compose up -d --build\""
   }
   triggers = {
     always_run = timestamp()
