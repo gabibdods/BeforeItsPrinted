a608809f82d1ff44f3ef8c0a2d683feae2656e02
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-21 16:02:53 -0400

    another debugging attempt

--- main.tf
+++ main.tf
@@ -5,22 +5,24 @@ resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
         command = <<EOT
             echo "Starting SSH command...";
-            ssh -vvv ${var.ssh_user}@${var.ssh_host} 'set -e;
-            export POSTGRES_USER="${var.postgres_user}";
-            export POSTGRES_PASSWORD="${var.postgres_pass}";
-            export PORT="${var.port}";
-            export TROP="${var.trop}";
-            cd "${var.app_dir}" &&
-            pwd &&
-            git pull &&
-            git status &&
-            docker compose down &&
-            docker ps &&
-            docker compose up -d --build' &&
-            docker ps -a;
+            ssh -vvv ${var.ssh_user}@${var.ssh_host} <<'EOF'
+                set -e
+                export POSTGRES_USER="${var.postgres_user}"
+                export POSTGRES_PASSWORD="${var.postgres_pass}"
+                export PORT="${var.port}"
+                export TROP="${var.trop}"
+                cd "${var.app_dir}"
+                pwd
+                git pull
+                git status
+                docker compose down
+                docker ps
+                docker compose up -d --build
+                docker ps -a
+            EOF
             echo "SSH command completed"
         EOT
-        interpreter = ["PowerShell", "-Command"]
+        interpreter = ["bash", "-c"]
     }
     triggers = {
         always_run = timestamp()
