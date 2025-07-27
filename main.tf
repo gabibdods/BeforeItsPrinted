f55f7f5ff68227f5d837c1f2e0df2aacde62625d
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:42:40 -0400

    added small debugging line

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
         interpreter = ["/bin/bash", "-c"]
         command = <<-EOT
+            echo user=>${var.user}<=
             sudo -u ${var.user} bash -lc '
                 cd "${var.dir}" &&
                 docker compose down &&
