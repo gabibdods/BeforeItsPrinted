19d6c414fc9650ea0dcca0aa35939ff13ee0b340
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:40:07 -0400

    fixed syntax error

--- main.tf
+++ main.tf
@@ -5,10 +5,12 @@ resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
         interpreter = ["/bin/bash", "-c"]
         command = <<-EOT
-            sudo -u ${var.user} bash -lc "cd ${var.dir}";
-            sudo -u ${var.user} docker compose down;
-            sudo -u ${var.user} docker compose up -d --build;
-            sudo -u ${var.user} docker ps;
+            sudo -u ${var.user} bash -lc '
+                cd "${var.dir}" &&
+                docker compose down &&
+                docker compose up -d --build &&
+                docker ps
+            '
         EOT
     }
     triggers = {
