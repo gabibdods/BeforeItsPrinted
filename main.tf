885f3058b6eae8adab275348919814ca3a173bbf
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:56:22 -0400

    rewrote main.tf for LF

--- main.tf
+++ main.tf
@@ -1,23 +1,22 @@
 variable "dir" {
-	type = string
+  type = string
 }
 variable "user" {
-	type = string
+  type = string
 }
 resource "null_resource" "deploy_compose" {
-    provisioner "local-exec" {
-        interpreter = ["/bin/bash", "-c"]
-        command = <<-EOT
-            echo "user=>${var.user}<="
-            sudo -u ${var.user} bash -lc '
-                cd "${var.dir}" &&
-                docker compose down &&
-                docker compose up -d --build &&
-                docker ps
-            '
-        EOT
-    }
-    triggers = {
-        always_run = timestamp()
-    }
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-xc"]
+    command = <<-EOT
+      sudo -u ${var.user} bash -lc '
+        cd "${var.dir}" &&
+        docker compose down &&
+        docker compose up -d --build &&
+        docker ps
+      '
+    EOT
+  }
+  triggers = {
+    always_run = timestamp()
+  }
 }
