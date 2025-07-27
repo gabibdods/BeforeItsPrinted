1bdc6400a16a679a7fca3814030ace626a5e3f9d
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:33:56 -0400

    created a new bash session as admin

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
         interpreter = ["/bin/bash", "-c"]
         command = <<-EOT
-            sudo -u ${var.user} cd "${var.dir}";
+            sudo -u ${var.user} bash -lc "cd ${var.dir}";
             sudo -u ${var.user} docker compose down;
             sudo -u ${var.user} docker compose up -d --build;
             sudo -u ${var.user} docker ps;
