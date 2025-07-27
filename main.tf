41a8a5ca4319af0412f596019e9df3c90485372e
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:12:30 -0400

    rewrote main.tf

--- main.tf
+++ main.tf
@@ -3,26 +3,13 @@ terraform {
 }
 resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
-        command = <<EOT
-            echo "Starting SSH command...";
-            ssh -vvv ${var.ssh_user}@${var.ssh_host} <<'EOF'
-                set -e
-                export POSTGRES_USER="${var.postgres_user}"
-                export POSTGRES_PASSWORD="${var.postgres_pass}"
-                export PORT="${var.port}"
-                export TROP="${var.trop}"
-                cd "${var.app_dir}"
-                pwd
-                git pull
-                git status
-                docker compose down
-                docker ps
-                docker compose up -d --build
-                docker ps -a
-            EOF
-            echo "SSH command completed"
-        EOT
-        interpreter = ["bash", "-c"]
+        interpreter = ["/bin/bash", "-c"]
+        inline = [
+            "cd \"${var.app_dir}\"",
+            "docker compose down",
+            "docker compose up -d --build",
+            "docker ps",
+        ]
     }
     triggers = {
         always_run = timestamp()
