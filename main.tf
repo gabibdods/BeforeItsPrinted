15196a7d3ee316925e2319ad3312ee1e306a0d52
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:13:20 -0400

    rewrote main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ terraform {
 resource "null_resource" "deploy_compose" {
     provisioner "local-exec" {
         interpreter = ["/bin/bash", "-c"]
-        inline = [
-            "cd \"${var.app_dir}\"",
-            "docker compose down",
-            "docker compose up -d --build",
-            "docker ps",
-        ]
+        command = <<-EOT
+            cd "${var.app_dir}";
+            docker compose down;
+            docker compose up -d --build;
+            docker ps;
+        EOT
     }
     triggers = {
         always_run = timestamp()
