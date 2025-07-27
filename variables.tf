4525e516a05f590879af2de367398db507186032
gabibdods <gabriel.oliveira.da.silva@outlook.com>
2025-07-27 02:17:42 -0400

    cleaned up a little

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "postgres_user" {
-	type = string
-}
-variable "postgres_pass" {
-	type = string
-}
-variable "ssh_user" {
-	type = string
-}
-variable "ssh_host" {
-	type = string
-}
 variable "app_dir" {
-	type = string
-}
-variable "port" {
-	type = string
-}
-variable "trop" {
 	type = string
 }
